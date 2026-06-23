8983c0e43a6e6cccee2d1a84341ff57f5d3d8e49
Abhishek Shrivastava <abhishekkumardipu@gmail.com>
2026-06-23 18:32:18 +0530

    Assets

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.soulfire.production',
+  appName: 'soulfire-chronicles-production',
+  webDir: 'public'
+};
+
+export default config;
